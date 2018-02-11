9f6a8741cfa583b25296053fd08c5e659a19a8bd
Nicko <nickojose3@yahoo.ca>
2018-02-11 14:16:48 -0600

    Updated DateTest with some string printouts

--- podcast/app/src/test/java/comp3350/podcast/tests/objects/DateTest.java
+++ podcast/app/src/test/java/comp3350/podcast/tests/objects/DateTest.java
@@ -34,22 +34,23 @@ public class DateTest {
 
     @Test
     public void equals() throws Exception {
-
+        System.out.println("\nStarting DateTest : match dates");
         assertTrue(blank.equals(blank));
         assertTrue(date.equals(dateCopy));
         assertFalse(blank.equals(date));
-
+        System.out.println("Finished DateTest : match dates");
     }
 
     @Test
     public void compareTo() throws Exception {
-
+        System.out.println("\nStarting DateTest : compare dates");
         assertEquals( blank.compareTo(blank), 0);
         assertEquals( date.compareTo(date), 0);
         assertEquals( date.compareTo(dateCopy), 0);
         assertEquals(date.compareTo(plusYear), -1);
         assertEquals( date.compareTo(plusSecond), -1);
         assertEquals( plusYear.compareTo(date), 1);
+        System.out.println("Finished DateTest : compare dates");
     }
 
 }
