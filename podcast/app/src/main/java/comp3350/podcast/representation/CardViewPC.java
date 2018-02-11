fb146525e8258d8f4482a888cccbe58b3f79d364
Nicko <nickojose3@yahoo.ca>
2018-02-10 23:56:57 -0600

    Added methods for storing and retrieving an episode.

--- podcast/app/src/main/java/comp3350/podcast/representation/CardViewPC.java
+++ podcast/app/src/main/java/comp3350/podcast/representation/CardViewPC.java
@@ -4,12 +4,15 @@ import android.content.Context;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
 
+import comp3350.podcast.objects.Episode;
+
 /**
  * Created by Gareth on 2018-02-01.
  */
 
-public class CardViewPC extends CardView {
+public class CardViewPC extends CardView{
     private String whoDis;
+    private Episode episode;
     public CardViewPC(Context context) {super(context);}
     public CardViewPC(Context context, AttributeSet attrs) {super(context, attrs);}
     public CardViewPC(Context context, AttributeSet attrs, int defStyleAttr) {super(context, attrs, defStyleAttr);}
@@ -17,7 +20,12 @@ public class CardViewPC extends CardView {
     public void setWhoDis(String whoDis){
         this.whoDis = whoDis;
     }
+    public void setEp(Episode episode) { this.episode = episode; }
 
+    public Episode getEp()
+    {
+        return episode;
+    }
     public String getWhoDis()
     {
         return whoDis;
