8577823790eacbd0e9af59b6250f4b6c9a306f61
Jackson B <barkerj3@myumanitoba.ca>
2018-02-11 15:25:03 -0600

    Added very basic channel info display to viewChannel

--- podcast/app/src/main/java/comp3350/podcast/representation/viewChannel.java
+++ podcast/app/src/main/java/comp3350/podcast/representation/viewChannel.java
@@ -15,9 +15,28 @@ import comp3350.podcast.objects.Episode;
 
 public class viewChannel extends AppCompatActivity {
 
+    private Channel channel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_channel);
+
+        channel = (Channel)getIntent().getSerializableExtra("channel");
+
+        updateText();
+    }
+
+    public void updateText()
+    {
+        TextView newText = (TextView) findViewById(R.id.channel_info);
+
+        newText.setText("Title:\t"+channel.getTitle()+"\n"
+                +"Author:\t"+channel.getAuthor()+"\n"
+                +"Category:\t"+channel.getCategory()+"\n"
+                +"Publish Date:\t"+channel.getPublishDate().toString()+"\n\n"
+                +channel.getDesc()+"\n\n"
+                +"Url:\t"+channel.getUrl());
+
+
     }
 }
