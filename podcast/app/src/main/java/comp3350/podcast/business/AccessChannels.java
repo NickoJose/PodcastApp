5368ea13352e12c0f7447c7d807cd782eddf3593
Themichaelreimer <themichaelreimer@gmail.com>
2018-02-14 09:37:55 -0600

    Enforced coding standards

--- podcast/app/src/main/java/comp3350/podcast/business/AccessChannels.java
+++ podcast/app/src/main/java/comp3350/podcast/business/AccessChannels.java
@@ -8,10 +8,6 @@ import comp3350.podcast.application.Services;
 import comp3350.podcast.objects.Channel;
 import comp3350.podcast.persistence.StubData;
 
-/**
- * Created by Almach on 2018-02-06.
- */
-
 public class AccessChannels
 {
     private StubData accessData;
@@ -27,12 +23,25 @@ public class AccessChannels
         currentChannel = 0;
     }
 
+    /**
+     * Puts all the channels in a playlist into a given List<Channel> object.
+     * The input/output follows a design pattern from the sample project.
+     *
+     * @param channels - list that we will store the result in
+     * @return - null. This pattern was taken from sample project
+     */
     public String getChannels(List<Channel> channels)
     {
         channels.clear();
         return accessData.getChannelSequential(channels);
     }
 
+    /**
+     * Gets the next channel in order. Functions similar to an iterator's get()
+     * The input/output follows a design pattern from the sample project.
+     *
+     * @return - next channel
+     */
     public Channel getSequential()
     {
         String result = null;
@@ -55,22 +64,52 @@ public class AccessChannels
         return channel;
     }
 
+    /**
+     * Sorts a channel list in-object, according to a given data field.
+     * Valid fields are: "title", "date".
+     * The input/output follows a design pattern from the sample project.
+     *
+     * @param channels - list that we are sorting
+     * @param type  - data field that we sort by. See description.
+     * @return - null. This pattern was taken from sample project
+     */
     public String sortChannel(List<Channel> channels, String type)
     {
         Sort.channel((ArrayList<Channel>)channels, type);
         return null;
     }
 
+    /**
+     * Inserts channel into database.
+     * The input/output follows a design pattern from the sample project.
+     *
+     * @param currentChannel - the channel we are inserting
+     * @return - null. This pattern was taken from sample project
+     */
     public String insertChannel(Channel currentChannel)
     {
         return accessData.insertChannel(currentChannel);
     }
 
+    /**
+     * Deletes a given channel from the database
+     * The input/output follows a design pattern from the sample project.
+     *
+     * @param currentChannel - the channel being removed from the database
+     * @return - null. This pattern was taken from sample project
+     */
     public String deleteChannel(Channel currentChannel)
     {
         return accessData.deleteChannel(currentChannel);
     }
 
+    /**
+     * Updates a channel in the database with a new version. Must have same title and URL to identify channel being updated.
+     * The input/output follows a design pattern from the sample project.
+     *
+     * @param currentChannel - channel being updated
+     * @return - null. This pattern was taken from sample project
+     */
     public String updateChannel(Channel currentChannel)
     {
         return accessData.updateChannel(currentChannel);
