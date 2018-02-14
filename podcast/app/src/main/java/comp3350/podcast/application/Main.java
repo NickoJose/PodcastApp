77569fb2d8945d82e37b65753f9d118eb695dc49
Themichaelreimer <themichaelreimer@gmail.com>
2018-02-14 10:02:59 -0600

    Coding standards

--- podcast/app/src/main/java/comp3350/podcast/application/Main.java
+++ podcast/app/src/main/java/comp3350/podcast/application/Main.java
@@ -1,9 +1,5 @@
 package comp3350.podcast.application;
 
-/**
- * Created by Almach on 2018-01-30.
- */
-
 public class Main
 {
     public static final String dbName = "Pod";
@@ -17,11 +13,22 @@ public class Main
         System.out.println("\nAll done");
     }
 
+    
+    /**
+     * Opens database for this session
+     *
+     * @return - void
+     */
     public static void startUp()
     {
         Services.createDataAccess(dbName);
     }
 
+    /**
+     * Closes database for this session
+     *
+     * @return - void
+     */
     public static void shutDown()
     {
         Services.closeDataAccess();
