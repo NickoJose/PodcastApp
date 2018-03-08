00950c13f66fac60fb523ac3cc88f8bd058047c5
GarethLW <33941421+GarethLW@users.noreply.github.com>
2018-03-08 14:55:10 -0600

    Improve search results

--- podcast/app/src/main/java/comp3350/podcast/business/Search.java
+++ podcast/app/src/main/java/comp3350/podcast/business/Search.java
@@ -16,16 +16,21 @@ public class Search {
     {
         LinkedList<Episode> out2 = new LinkedList<>();
         int l;
-        int bestL = 0;
+        int bestL = key.length()*2;
+        int minL = key.length();
 
         // not the best way to sort
         for(Episode a : in)
         {
-            l = lcs(a.getTitle(), key).length();
+            l = matchHeuristic(a.getTitle(), key);
 
             if (l >= bestL) {
                 out2.addFirst(a);
                 bestL = l;
+            }
+            else if (l >= minL)
+            {
+                out2.addLast(a);
             }
 
         }
@@ -34,22 +39,41 @@ public class Search {
     }
 
     /**
-     * Computes the longest common subsequence between two strings
+     * Creates a heuristic for the similarity between two strings using the length of
+     * the longest common subsequence and substring
      *
      * @return - String containing the longest common subsequence
      */
-    public static String lcs(String a, String b) {
+    public static int matchHeuristic (String a, String b) {
         a = a.toLowerCase();
         b = b.toLowerCase();
+
+        int m = a.length();
+        int n = b.length();
+
+        int max = 0;
+
+        int[][] dp = new int[m][n];
         int[][] lengths = new int[a.length()+1][b.length()+1];
 
         for (int i = 0; i < a.length(); i++)
-            for (int j = 0; j < b.length(); j++)
+            for (int j = 0; j < b.length(); j++) {
+                if (a.charAt(i) == b.charAt(j)) {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                    }
+
+                    if (max < dp[i][j])
+                        max = dp[i][j];
+                }
                 if (a.charAt(i) == b.charAt(j))
-                    lengths[i+1][j+1] = lengths[i][j] + 1;
+                    lengths[i + 1][j + 1] = lengths[i][j] + 1;
                 else
-                    lengths[i+1][j+1] =
-                            Math.max(lengths[i+1][j], lengths[i][j+1]);
+                    lengths[i + 1][j + 1] =
+                            Math.max(lengths[i + 1][j], lengths[i][j + 1]);
+            }
 
         // recover string from matrix
         StringBuffer sb = new StringBuffer();
@@ -67,6 +91,7 @@ public class Search {
             }
         }
 
-        return sb.reverse().toString();
+        String lcsub = sb.reverse().toString();
+        return lcsub.length() + max*2;
     }
 }
