be02344ac21870cd232e517ae60b3db5f5b25184
Nicko <nickojose3@yahoo.ca>
2018-03-12 20:35:40 -0500

    Added comments for subscription interface

--- podcast/app/src/main/java/comp3350/podcast/business/AccessSubscriptions.java
+++ podcast/app/src/main/java/comp3350/podcast/business/AccessSubscriptions.java
@@ -17,17 +17,38 @@ public class AccessSubscriptions
         accessData = Services.getDataAccess(Main.dbName);
     }
 
+    /**
+     * Puts all the subscriptions into a given List<Channel> object.
+     * The input/output follows a design pattern from the sample project.
+     *
+     * @param subs - list that we will store the result in
+     * @return - null. This pattern was taken from sample project
+     */
     public String getSubs(List<Channel> subs)
     {
         subs.clear();
         return accessData.getSubSequential(subs);
     }
 
+    /**
+     * Inserts subscription into database.
+     * The input/output follows a design pattern from the sample project.
+     *
+     * @param currentChannel - the channel we are inserting
+     * @return - null. This pattern was taken from sample project
+     */
     public String insertSub(Channel currentChannel)
     {
         return accessData.insertSub(currentChannel);
     }
 
+    /**
+     * Deletes a given subscription from the database
+     * The input/output follows a design pattern from the sample project.
+     *
+     * @param currentChannel - the channel being removed from the database
+     * @return - null. This pattern was taken from sample project
+     */
     public String deleteSub(Channel currentChannel)
     {
         return accessData.deleteSub(currentChannel);
