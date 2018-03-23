b32512c77212a35595a426fa7e01f332ab002bc2
Jackson <barkerj3@myumanitoba.ca>
2018-03-23 11:42:27 -0500

    Copied acceptance test hierarchy from SRSYS and added required .jar file for robotium

---
+++ podcast/app/src/androidTest/java/comp3350/podcast/acceptance/AcceptanceTests.java
@@ -0,0 +1,15 @@
+package comp3350.podcast.acceptance;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AcceptanceTests
+{
+	public static TestSuite suite;
+
+    public static Test suite()
+    {
+        suite = new TestSuite("Acceptance tests");
+        return suite;
+    }
+}
