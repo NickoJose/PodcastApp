795b5791e82feac9e81a23880ad00ef94d1e8256
Jackson B <barkerj3@myumanitoba.ca>
2018-03-27 16:33:43 -0500

    Reenabled tests for playlists

--- podcast/app/src/androidTest/java/comp3350/podcast/acceptance/AcceptanceTests.java
+++ podcast/app/src/androidTest/java/comp3350/podcast/acceptance/AcceptanceTests.java
@@ -12,7 +12,7 @@ public class AcceptanceTests
         suite = new TestSuite("Acceptance tests");
         suite.addTestSuite(MainTest.class);
         suite.addTestSuite(SearchTest.class);
-        //suite.addTestSuite(PlaylistTest.class);
+        suite.addTestSuite(PlaylistTest.class);
         suite.addTestSuite(SubscriptionTest.class);
         return suite;
     }
